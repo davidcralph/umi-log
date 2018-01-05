e692513c218d4c0b0705c468906debf528d4b6d7
Dave R <14052956+ohlookitsderpy@users.noreply.github.com>
2018-01-05 12:04:06 +0000

    Update log.js

--- log.js
+++ log.js
@@ -1,22 +1,22 @@
 const style = require('ansi-styles');
 const d = new Date();
-const time = `[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}]`
+const time = `[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}]`;
 
 class log {
     static notify (message) {
-        return console.log(`${time} - ${style.bgGreen.open}NOTIFY${style.bgGreen.close}:`, `${style.green.open}${message}${style.green.close}`)
+        return console.log(`${time} - ${style.bgGreen.open}NOTIFY${style.bgGreen.close}:`, `${style.green.open}${message}${style.green.close}`);
     }
     static warn (message) {
-        return console.log(`${time} - ${style.bgYellow.open}WARN${style.bgYellow.close}:`, `${style.yellow.open}${message}${style.yellow.close}`)
+        return console.log(`${time} - ${style.bgYellow.open}WARN${style.bgYellow.close}:`, `${style.yellow.open}${message}${style.yellow.close}`);
     }
     static error (message) {
-        return console.log(`${time} - ${style.bgRed.open}ERROR${style.bgRed.close}:`, `${style.red.open}${message}${style.red.close}`)
+        return console.log(`${time} - ${style.bgRed.open}ERROR${style.bgRed.close}:`, `${style.red.open}${message}${style.red.close}`);
     }
     static debug (message) {
-        return console.log(`${time} - ${style.bgMagenta.open}DEBUG${style.bgMagenta.close}:`, `${style.magenta.open}${message}${style.magenta.close}`)
+        return console.log(`${time} - ${style.bgMagenta.open}DEBUG${style.bgMagenta.close}:`, `${style.magenta.open}${message}${style.magenta.close}`);
     } 
     static custom (title, message) {
-        return console.log(`${time} - ${style.bgBlackBright.open}${title}${style.bgBlackBright.close}:`, `${style.white.open}${message}${style.white.close}`)
+        return console.log(`${time} - ${style.bgBlackBright.open}${title}${style.bgBlackBright.close}:`, `${style.white.open}${message}${style.white.close}`);
     }     
 }
 
