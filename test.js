2cbe92a8eda65d449205317f5f75d0f2df9db09a
Dave R <14052956+ohlookitsderpy@users.noreply.github.com>
2018-02-23 18:20:34 +0000

    1.1.2

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const log = require("./log.js");
 
 log.info("This is a test");
+log.notify("This is a test");
 log.warn("This is a test");
 log.error("This is a test");
 log.debug("This is a test");
