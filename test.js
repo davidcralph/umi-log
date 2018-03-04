9d87f46228f899fb9d81eb498e4735e20ab6560a
Dave R <14052956+ohlookitsderpy@users.noreply.github.com>
2018-03-04 13:13:04 +0000

    2.0.0

--- test.js
+++ test.js
@@ -5,4 +5,4 @@ log.notify("This is a test");
 log.warn("This is a test");
 log.error("This is a test");
 log.debug("This is a test");
-log.custom("Test", "This is a test");
+log.custom("TEST", "This is a test");
