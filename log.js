1f3b26bf16fd15c7277c69b0004aefee7b8cacdd
Dave R <14052956+ohlookitsderpy@users.noreply.github.com>
2018-03-31 13:42:20 +0100

    2.0,2

--- log.js
+++ log.js
@@ -4,11 +4,10 @@ const time = `[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}]`;
 
 class log {
     static info (message) { return console.log(`${time} - ${colors.bgGreen("INFO")}: ${colors.green(message)}`); }
-    static notify (message) { return console.log(`${time} - ${colors.bgGreen("INFO")}: ${colors.green(message)}`); }
     static warn (message) { return console.log(`${time} - ${colors.bgYellow("WARN")}: ${colors.yellow(message)}`); }
     static error (message) { return console.log(`${time} - ${colors.bgRed("ERROR")}: ${colors.red(message)}`); }
-    static debug (message) { return console.log(`${time} - ${colors.bgMagenta("DEBUG")}: ${colors.magenta(message)}`); } 
-    static custom (title, message) { return console.log(`${time} - ${colors.bgBrightBlack(title)}: ${colors.white(message)}`); }     
+    static debug (message) { return console.log(`${time} - ${colors.bgMagenta("DEBUG")}: ${colors.magenta(message)}`); }
+    static custom (title, message) { return console.log(`${time} - ${colors.bgBrightBlack(title)}: ${colors.white(message)}`); }
 }
 
 module.exports = log;
