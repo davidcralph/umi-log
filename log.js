d38e4cf156bd058d1c89c6c40b7929e2410250c5
Dave R <14052956+ohlookitsderpy@users.noreply.github.com>
2018-04-03 16:16:14 +0100

    3.0.0

--- log.js
+++ log.js
@@ -1,13 +1,12 @@
-const colors = require("ansicolors");
 const d = new Date();
 const time = `[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}]`;
 
 class log {
-    static info (message) { return console.log(`${time} - ${colors.bgGreen("INFO")}: ${colors.green(message)}`); }
-    static warn (message) { return console.log(`${time} - ${colors.bgYellow("WARN")}: ${colors.yellow(message)}`); }
-    static error (message) { return console.log(`${time} - ${colors.bgRed("ERROR")}: ${colors.red(message)}`); }
-    static debug (message) { return console.log(`${time} - ${colors.bgMagenta("DEBUG")}: ${colors.magenta(message)}`); }
-    static custom (title, message) { return console.log(`${time} - ${colors.bgBrightBlack(title)}: ${colors.white(message)}`); }
+    static info (message) { return console.log(`${time} - \x1b[42mINFO\x1b[0m: \x1b[32m${message}\x1b[0m`) }
+    static warn (message) { return console.log(`${time} - \x1b[43mWARN\x1b[0m: \x1b[33m${message}\x1b[0m`); }
+    static error (message) { return console.log(`${time} - \x1b[41mERROR\x1b[0m: \x1b[31m${message}\x1b[0m`); }
+    static debug (message) { return console.log(`${time} - \x1b[45mDEBUG\x1b[0m: \x1b[35m${message}\x1b[0m`); }
+    static custom (title, message) { return console.log(`${time} - \x1b[100m${title}\x1b[0m: ${message}`); }
 }
 
 module.exports = log;
