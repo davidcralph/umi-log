7aaa95306fac9b85668dde7aae0def64f68fdeee
Dave R <14052956+ohlookitsderpy@users.noreply.github.com>
2018-04-03 16:25:55 +0100

    3.0.1

--- log.js
+++ log.js
@@ -2,7 +2,7 @@ const d = new Date();
 const time = `[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}]`;
 
 class log {
-    static info (message) { return console.log(`${time} - \x1b[42mINFO\x1b[0m: \x1b[32m${message}\x1b[0m`) }
+    static info (message) { return console.log(`${time} - \x1b[42mINFO\x1b[0m: \x1b[32m${message}\x1b[0m`); }
     static warn (message) { return console.log(`${time} - \x1b[43mWARN\x1b[0m: \x1b[33m${message}\x1b[0m`); }
     static error (message) { return console.log(`${time} - \x1b[41mERROR\x1b[0m: \x1b[31m${message}\x1b[0m`); }
     static debug (message) { return console.log(`${time} - \x1b[45mDEBUG\x1b[0m: \x1b[35m${message}\x1b[0m`); }
