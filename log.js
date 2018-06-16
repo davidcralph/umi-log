4eb36dbf3ce356b3af4245aa4c0e0a9171288567
Dave R <davetheprogrammer2@gmail.com>
2018-06-16 10:12:21 +0100

    3.0.2

--- log.js
+++ log.js
@@ -1,12 +1,13 @@
-const d = new Date();
-const time = `[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}]`;
+var d = new Date();
+var t = `[${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}]`;
+var c = console;
 
 class log {
-    static info (message) { return console.log(`${time} - \x1b[42mINFO\x1b[0m: \x1b[32m${message}\x1b[0m`); }
-    static warn (message) { return console.log(`${time} - \x1b[43mWARN\x1b[0m: \x1b[33m${message}\x1b[0m`); }
-    static error (message) { return console.log(`${time} - \x1b[41mERROR\x1b[0m: \x1b[31m${message}\x1b[0m`); }
-    static debug (message) { return console.log(`${time} - \x1b[45mDEBUG\x1b[0m: \x1b[35m${message}\x1b[0m`); }
-    static custom (title, message) { return console.log(`${time} - \x1b[100m${title}\x1b[0m: ${message}`); }
+    static info (m) { return c.log(`${t} - \x1b[42mINFO\x1b[0m: \x1b[32m${m}\x1b[0m`); }
+    static warn (m) { return c.log(`${t} - \x1b[43mWARN\x1b[0m: \x1b[33m${m}\x1b[0m`); }
+    static error (m) { return c.log(`${t} - \x1b[41mERROR\x1b[0m: \x1b[31m${m}\x1b[0m`); }
+    static debug (m) { return c.log(`${t} - \x1b[45mDEBUG\x1b[0m: \x1b[35m${m}\x1b[0m`); }
+    static custom (ti, m) { return c.log(`${t} - \x1b[100m${ti}\x1b[0m: ${m}`); }
 }
 
 module.exports = log;
